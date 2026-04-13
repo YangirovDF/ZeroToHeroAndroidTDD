e46cf4c750eb886c8ec83036e3190e7ded66cace
Denis Yangirov <yangirovdf@gmail.ru>
2026-04-13 09:56:01 +0300

    solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -6,13 +6,9 @@ import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    companion object {
-        const val KEY_INSTANCE_STATE = "KeyInstanceState"
-    }
 
     private lateinit var tv: TextView
     private lateinit var btn: Button
-    private var textWhoAmI: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,21 +17,10 @@ class MainActivity : AppCompatActivity() {
         tv = findViewById(R.id.titleTextView)
         btn = findViewById(R.id.changeButton)
 
-
-        if (savedInstanceState != null) {
-            textWhoAmI = savedInstanceState.getString(KEY_INSTANCE_STATE) ?: ""
-            tv.text = textWhoAmI
-        }
-
         btn.setOnClickListener {
-            textWhoAmI = "I am an Android Developer!"
-            tv.text = textWhoAmI
+            tv.text = "I am an Android Developer!"
         }
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putString(KEY_INSTANCE_STATE, textWhoAmI)
-    }
 
 }
