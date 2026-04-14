18c3cddf0e2a1a23fa72a0772899cc6171496225
Denis Yangirov <yangirovdf@gmail.ru>
2026-04-14 10:44:05 +0300

    решение

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,28 +12,39 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
 
     private var tv: TextView? = null
+    private lateinit var linearLayout: LinearLayout
     private lateinit var btn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        linearLayout = findViewById<LinearLayout>(R.id.rootLayout)
         tv = findViewById(R.id.titleTextView)
         btn = findViewById(R.id.removeButton)
 
-        savedInstanceState?.let {
-            if (!savedInstanceState.getBoolean("key")) {
-                (tv?.parent as ViewGroup).removeView(tv)
-            }
-        }
 
         btn.setOnClickListener {
             try {
-                (tv?.parent as ViewGroup).removeView(tv)
+                linearLayout.removeView(tv)
             } catch (e: Exception) {
                 Log.e("MainActivity", e.toString())
             }
         }
+    }
+
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val removedTextView = savedInstanceState.getBoolean("key")
+        if (removedTextView) {
+            linearLayout.removeView(tv)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val removedTextView = linearLayout.childCount == 1
+        outState.putBoolean("key", removedTextView)
     }
 
 }
