6bf68cd12527765b38516b4f71c56c3056c94aa0
Denis Yangirov <yangirovdf@gmail.ru>
2026-04-14 07:40:12 +0300

    solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var tv: TextView
     private lateinit var btn: Button
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,6 +21,15 @@ class MainActivity : AppCompatActivity() {
         btn.setOnClickListener {
             tv.text = "I am an Android Developer!"
         }
+        savedInstanceState?.let {
+            tv.text = savedInstanceState.getString("key")
+        }
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("key", tv.text.toString())
     }
 
 
