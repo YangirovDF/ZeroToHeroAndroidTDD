cc9a6879e69f1fecda171569ca38bcd62580b135
Denis Yangirov <yangirovdf@gmail.ru>
2026-04-14 10:30:02 +0300

    плохо, но тест проходит

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,38 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var tv: TextView? = null
+    private lateinit var btn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tv = findViewById(R.id.titleTextView)
+        btn = findViewById(R.id.removeButton)
+
+        savedInstanceState?.let {
+            if (!savedInstanceState.getBoolean("key")) {
+                (tv?.parent as ViewGroup).removeView(tv)
+            }
+        }
+
+        btn.setOnClickListener {
+            try {
+                (tv?.parent as ViewGroup).removeView(tv)
+            } catch (e: Exception) {
+                Log.e("MainActivity", e.toString())
+            }
+        }
     }
+
 }
